4c20ee8d506dcbb0af248f68d66d6a166e4d1d36
Turner Van Duser <turner.vanduser@gmail.com>
2025-04-15 02:22:26 -0500

    took away everything that i just tried to do, no lsp progress has been made

--- lua/turner/init.lua
+++ lua/turner/init.lua
@@ -1,3 +1,3 @@
 require("turner.remap")
 require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
-require("turner.set")
+require("turner.options")
--- lua/turner/lazy.lua
+++ lua/turner/lazy.lua
@@ -22,7 +22,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
--- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     -- import your plugins
@@ -30,7 +29,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "elflord" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })

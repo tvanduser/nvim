aa3e01cfde6b636aba1146ef39b6ade1f580f3e0
Turner Van Duser <turner.vanduser@gmail.com>
2025-04-15 20:44:09 -0500

    made the color schemes better and gave some options, you change in the colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,11 +1,50 @@
+-- Pick your preferred theme here
+local colorscheme = "catppuccin" -- options: "gruvbox", "tokyonight", "onedark", etc.
+
 return {
-{
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+  -- Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    cond = function() return colorscheme == "catppuccin" end,
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme elflord]])
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  -- Gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    cond = function() return colorscheme == "gruvbox" end,
+    config = function()
+      vim.cmd.colorscheme("gruvbox")
+    end,
+  },
+
+  -- TokyoNight
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    cond = function() return colorscheme == "tokyonight" end,
+    config = function()
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+
+  -- OneDark
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    cond = function() return colorscheme == "onedark" end,
+    config = function()
+      vim.cmd.colorscheme("onedark")
     end,
   },
 }
+
--- lua/turner/remap.lua
+++ lua/turner/remap.lua
@@ -15,3 +15,9 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 vim.keymap.set('n', '<space>c', ':lclose<CR>', { noremap = true, silent = true, desc = 'Close diagnostics list' })
+
+--keybindings for selecting autocomplete items
+--<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+--['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+--['<C-y>'] = cmp.mapping.confirm({ select = true }),
+--["<C-Space>"] = cmp.mapping.complete(),

4e4a1a2a4d4a6a78bec16c1d8629265569f86fb4
Turner Van Duser <turner.vanduser@gmail.com>
2025-04-29 11:36:10 -0500

    added lazy git
    added lazygit support to provide a gui when working with git

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,11 @@
+return {
+  "kdheepak/lazygit.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  cmd = { "LazyGit" },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" }
+  }
+}
+

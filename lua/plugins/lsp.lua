a97240c0e38c1bd23a885549d688ea8fd1643073
Turner Van Duser <turner.vanduser@gmail.com>
2026-04-09 10:48:36 -0500

    updated lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,198 +1,60 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
     },
 
     config = function()
-        -- Set up LSP formatting
-        require("conform").setup({
-            formatters_by_ft = {
-                c = { "clang-format" },
-                cpp = { "clang-format" },
-                lua = { "stylua" },
-                java = { "jdtls"},
-                python = { "black", "isort" },
-            }
-        })
-
-        -- Completion setup
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        -- Define a common on_attach function for LSPs
-        local on_attach = function(client, bufnr)
-            local bufopts = { noremap = true, silent = true, buffer = bufnr }
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-            vim.keymap.set('n', '<space>wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, bufopts)
-            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-            vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts) vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts) vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts) end -- Set up LSP status indicator require("fidget").setup({})
-        
-        -- Set up Mason package manager for LSP servers
+        -- Mason: install LSP servers
         require("mason").setup()
-        
-        -- Configure Mason LSP servers
-        -- ****** PUT THE NEW LANGUAGES SERVERS INSTALLED WITH MASON HERE *************
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                "clangd",
-                "lua_ls",
-                "harper_ls",
-                "jdtls",
-                "pyright",
-            },
-            handlers = {
-                function(server_name) -- default handler
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities,
-                        on_attach = on_attach
-                    }
-                end,
-
-                -- Specific handler for clangd
-                clangd = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.clangd.setup({
-                        capabilities = capabilities,
-                        on_attach = function(client, bufnr)
-                            on_attach(client, bufnr)
-                            -- Additional C-specific keybindings
-                            vim.keymap.set("n", "<leader>h", "<cmd>ClangdSwitchSourceHeader<cr>", 
-                                          { buffer = bufnr, desc = "Switch Source/Header" })
-                        end,
-                        cmd = {
-                            "clangd",
-                            "--background-index",
-                            "--suggest-missing-includes",
-                            "--clang-tidy",
-                            "--header-insertion=iwyu",
-                        },
-                        init_options = {
-                            clangdFileStatus = true,
-                        },
-                    })
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = false,
-                                enable_snippets = true,
-                                warn_style = false,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                        settings = {
-                            Lua = {
-                                format = {
-                                    enable = true,
-                                    defaultConfig = {
-                                        indent_style = "space",
-                                        indent_size = "2",
-                                    }
-                                },
-                                diagnostics = {
-                                    globals = { 'vim' }  -- Recognize vim global in lua files
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
+            ensure_installed = { "pyright" },
         })
 
-        -- Configure completion behavior
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        -- Capabilities for autocomplete
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
+        -- Keymaps when LSP attaches
+        local on_attach = function(_, bufnr)
+            local opts = { noremap = true, silent = true, buffer = bufnr }
+            vim.keymap.set('n', 'gd',        vim.lsp.buf.definition,    opts)
+            vim.keymap.set('n', 'K',         vim.lsp.buf.hover,         opts)
+            vim.keymap.set('n', 'gr',        vim.lsp.buf.references,    opts)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,       opts)
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action,  opts)
+        end
 
-    mapping = {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+        -- Set up pyright
+        require("lspconfig").pyright.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+        -- Autocomplete
+        local cmp = require("cmp")
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ["<Tab>"]   = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                ["<CR>"]    = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+            },
+        })
 
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    },
-
-    sources = cmp.config.sources({
-        { name = "copilot", group_index = 2 },
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-    }, {
-        { name = "buffer" },
-    }),
-})
-
-        -- Configure diagnostics display
+        -- Diagnostics
         vim.diagnostic.config({
-            update_in_insert = false,  -- Update diagnostics while typing
-            virtual_text = true,      -- Show errors inline
-            signs = true,             -- Show signs in the gutter
-            underline = true,         -- Underline problematic code
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
         })
-
-    end
+    end,
 }
